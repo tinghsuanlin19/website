e6b2f3d7f3111c0d1fd9d36f6914a054f1592959
tinghsuanlin19 <tinghsuanlin.n@gmail.com>
2024-06-02 18:49:28 +0200

    Add files via upload

---
+++ video.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var playIcon = 'https://uploads-ssl.webflow.com/5dde919caf313a3410908cfd/665c79405109ee93b0840805_icon_play_dark.svg'; // URL of play icon
+  var pauseIcon = 'https://uploads-ssl.webflow.com/5dde919caf313a3410908cfd/665c793fa7680cd7c30b1816_icon_pause_dark.svg'; // URL of pause icon
+  var offset = window.innerHeight * 0.20; // 20% offset
+
+  document.querySelectorAll('.myVideo').forEach((video, index) => {
+    var button = document.querySelectorAll('.play-pause-button')[index];
+    var hasPlayed = false; // Track if the video has started playing
+
+    // Play video when first in view
+    function checkVideoVisibility() {
+      var rect = video.getBoundingClientRect();
+      var isVisible = rect.top >= -offset && rect.bottom <= window.innerHeight + offset;
+
+      if (isVisible && !hasPlayed) {
+        video.play();
+        button.src = pauseIcon;
+        hasPlayed = true; // Mark video as having started
+      }
+    }
+
+    // Change button state when video ends
+    video.addEventListener('ended', () => {
+      button.src = playIcon;
+      // Remove the following line to prevent resetting hasPlayed
+      // hasPlayed = false; // Allow video to be played again when scrolled into view
+    });
+
+    // Toggle play/pause on button click
+    button.addEventListener('click', () => {
+      if (video.paused) {
+        video.play();
+        button.src = pauseIcon;
+      } else {
+        video.pause();
+        button.src = playIcon;
+      }
+    });
+
+    window.addEventListener('scroll', checkVideoVisibility);
+    window.addEventListener('resize', checkVideoVisibility);
+
+    checkVideoVisibility(); // Initial check
+  });
+});
